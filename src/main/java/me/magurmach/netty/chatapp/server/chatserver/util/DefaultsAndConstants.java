cc06997f28ed6366e29e7076fe77150013f5c4de
Shakib Ahmed <sheikahm@cisco.com>
2017-12-29 14:53:06 -0800

    Add default config POJO

---
+++ src/main/java/me/magurmach/netty/chatapp/server/chatserver/util/DefaultsAndConstants.java
@@ -0,0 +1,10 @@
+package me.magurmach.netty.chatapp.server.chatserver.util;
+
+/**
+ * Created by Shakib Ahmed on 12/26/17.
+ */
+public class DefaultsAndConstants {
+    public static final String DEFAULT_CHAT_END_LIMITER = "bye";
+    public static final String DEFAULT_HOST = "localhost";
+    public static final int DEFAULT_PORT = 8080;
+}
